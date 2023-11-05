f593f3c0cfe4f973660b201c16182b2710bef281
LeLong <21130436@st.hcmuaf.edu.vn>
2023-11-05 20:21:44 +0700

    task4-Hill_climbing

--- Lab5/src/Task_1/HillClimbing.java
+++ Lab5/src/Task_1/HillClimbing.java
@@ -15,24 +15,30 @@ public class HillClimbing implements IPuzzleAlgo{
 		
 	    while (true) { 
 	// save the node has been traverses
+	    	if(current.getH() == 0) return current;
 	    	explored.add(current);
 	    	
-	    	if(current.getH() == 0) return current;
-	    	PriorityQueue<Node> neighbors = new PriorityQueue<Node>(PuzzleUtils.HeuristicComparatorByF);
+	    	PriorityQueue<Node> neighbors = new PriorityQueue<Node>(PuzzleUtils.HeuristicComparatorByH);
 	    	
 	// Add neighbor of current to neighbors and sort by H1
 	    	for(Node children : model.getSuccessors(current)) {
-	    		children.setG(current.getG() + children.getH() - current.getH() + 1);
-	    		neighbors.add(children);
+	    		if(!explored.contains(children)) {
+		    		neighbors.add(children);
+	    		}
 	    	}
 	    	
 	// take the best choice from neighbor and check whether or not the node more performance than current node
 	    	Node tmp = neighbors.poll();
-	    	if(current.getF() >= tmp.getF()) {
-	    		current = tmp;
+	    	if(current.getH() > tmp.getH()) {
+	    		current = tmp ;
+	    		
 	    	}
-	    	else return current;
+	    	else {
+	    		break;
+	    	}
+	    	
 	    }
+	    return current;
 	}
 
 }
